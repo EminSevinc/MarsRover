2b74f2e4f7366db559ce2c411ec1856cde5c79a4
Mikro23 <eminsevinc85@hotmail.com>
2021-04-29 22:22:25 +0300

    Add files via upload

---
+++ MarsRoverConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using MarsRover.Library;
+using MarsRover.Library.Helper;
+using MarsRover.Library.Interface;
+using System;
+using System.Collections.Generic;
+using Validator;
+
+namespace MarsRoverConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Inputs:
+            //5 5
+            //1 2 N
+            //LMLMLMLMM
+            //Expected Output:
+            //1 3 N
+
+            //Test Inputs:
+            //3 3 E
+            //MMRMMRMRRM
+            //Expected Output:
+            //5 1 E
+
+            Console.WriteLine("Please enter X and Y coordinates of surface (5 5) :");
+            string surfaceCoordinates = Console.ReadLine();
+            Validation.ValidateCoordinates(surfaceCoordinates);
+            ICoordinate surface = new Surface(surfaceCoordinates);
+
+            Console.WriteLine("\nPlease enter the location (X Y) and direction (N, W, E or S) you want to land the rover (1 2 N) :");
+            string roverArguments = Console.ReadLine();
+            Validation.ValidateCoordinatesAndDirection(roverArguments);
+            IRover rover = new Rover(roverArguments, surface);
+
+            Console.WriteLine("\nSurface was created and rover was landed succesfully\n");
+
+            do
+            {
+                Console.WriteLine("\nPlease enter the commands (M, L or R) you want to move the rover :");
+                string moves = Console.ReadLine();
+                List<string> movements = Movement.BuildMovement(moves);
+
+                foreach (var movement in movements)
+                {
+                    rover.Move(movement);
+                }                
+
+            } while (true);
+        }
+    }
+}
